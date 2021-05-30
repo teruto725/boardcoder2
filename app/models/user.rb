487a90753f8171920d2bccf5f33cc420ce93086c
teruto725 <akiteruto@gmail.com>
2021-05-30 15:29:15 +0900

    add name to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   mount_uploader :image, ImageUploader
+  validates :name,
+            presence: true, #追記
+            uniqueness: true
 end
